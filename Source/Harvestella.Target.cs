f507bd50a85f9d7374898e9d8f81e313ffbda832
Narknon <narknon0@gmail.com>
2022-11-09 18:22:20 -0500

    remove hotreload dlls

---
+++ Source/Harvestella.Target.cs
@@ -0,0 +1,13 @@
+﻿using UnrealBuildTool;
+
+public class HarvestellaTarget : TargetRules {
+	public HarvestellaTarget(TargetInfo Target) : base(Target) {
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange(new string[] {
+			"LwSys",
+			"Shinsen",
+			"ShinsenMd",
+		});
+	}
+}
